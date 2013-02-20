f0eb68a65835dcecfdcf8ddfcf0cd7507523aa3a
Johnathan Gilday <johnathan.gilday@nextcentury.com>
2013-02-20 16:41:16 -0500

    Initial Commit

---
+++ ast-definitions/src/main/groovy/org/example/GreetsASTTransformation.groovy
@@ -0,0 +1,66 @@
+/* 
+   Copyright 2013 Next Century Corporation 
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package org.example
+
+import org.codehaus.groovy.ast.ASTNode
+import org.codehaus.groovy.ast.AnnotatedNode
+import org.codehaus.groovy.ast.AnnotationNode
+import org.codehaus.groovy.ast.ClassHelper
+import org.codehaus.groovy.ast.ClassNode
+import org.codehaus.groovy.ast.MethodNode
+import org.codehaus.groovy.ast.Parameter
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.ConstructorCallExpression
+import org.codehaus.groovy.ast.stmt.BlockStatement
+import org.codehaus.groovy.ast.stmt.ReturnStatement
+import org.codehaus.groovy.ast.stmt.ThrowStatement
+import org.codehaus.groovy.control.CompilePhase
+import org.codehaus.groovy.control.SourceUnit
+import org.codehaus.groovy.transform.ASTTransformation
+import org.codehaus.groovy.transform.GroovyASTTransformation
+
+@GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
+class GreetsASTTransformation implements ASTTransformation {
+
+    @Override
+    void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
+        if (!astNodes) return
+        if (!astNodes[0]) return
+        if (!astNodes[1]) return
+        if (!(astNodes[0] instanceof AnnotationNode)) return
+        if (astNodes[0].classNode?.name != Greets.class.name) return
+        if (!(astNodes[1] instanceof ClassNode)) return
+
+        ClassNode classNode = (ClassNode)astNodes[1]
+        classNode.addMethod(createMethod())
+        println "Added method"
+    }
+
+    private MethodNode createMethod() {
+        def sayHelloBlock = new BlockStatement()
+        sayHelloBlock.addStatement(new ReturnStatement(new ConstantExpression('Hello World!')))
+
+        new MethodNode(
+                'sayHello',
+                MethodNode.ACC_PUBLIC,
+                ClassHelper.STRING_TYPE,
+                Parameter.EMPTY_ARRAY,
+                ClassNode.EMPTY_ARRAY,
+                sayHelloBlock
+        )
+    }
+}
